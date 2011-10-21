0ba2fb15e7b134c830a85caea052a3c18b4c8efa
the.eric.liao@gmail.com <the.eric.liao@gmail.com@fba0463a-ef31-6052-f655-2c831954ceaf>
2011-10-21 16:40:08 +0000

    Added PREMIS metadata to object model + display
    git-svn-id: http://bril.googlecode.com/svn/trunk@40 fba0463a-ef31-6052-f655-2c831954ceaf

--- mybril/repo/models.py
+++ mybril/repo/models.py
@@ -10,6 +10,7 @@ class FileObject(DigitalObject):
     CONTENT_MODELS = [ FILE_CONTENT_MODEL ]
     file = FileDatastream("MYDS", "Binary datastream", defaults={'versionable': True,})
     brilmeta = Datastream("BRILMETA", "BRIL Metadata", defaults={'versionable': True,})
+    PREMISmeta = Datastream("PREMIS", "PREMIS Object Metadata", defaults={'versionable': True,})
     
 class AggregationObject(DigitalObject):
     rdfa = Datastream("RDFA", "OAI-ORE RDFa Representation", defaults={'versionable': True,})
